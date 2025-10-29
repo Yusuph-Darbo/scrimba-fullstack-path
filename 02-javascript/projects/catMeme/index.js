dd2d2fe6b5191ab74a05adb12aeb4ef590550fc9
Yusuph <yusuphdarbo@gmail.com>
2025-10-29 13:39:38 +0000

    Base js logic

--- 02-javascript/projects/catMeme/index.js
+++ 02-javascript/projects/catMeme/index.js
@@ -1,1 +1,14 @@
 import { catsData } from '/data.js'
+
+const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+getImageBtn.addEventListener('click', renderCat)
