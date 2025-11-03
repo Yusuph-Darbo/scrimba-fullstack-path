fe466971f392a01f555d6ace262f39301b1b49d6
Yusuph <yusuphdarbo@gmail.com>
2025-11-03 09:22:59 +0000

    Created html boilerplate

--- 02-javascript/projects/twimba/index.js
+++ 02-javascript/projects/twimba/index.js
@@ -1,5 +1,62 @@
-import { tweetsData } from './data.js';
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+import { tweetsData } from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
+
+function getFeedHtml(){
+    let feedHtml = ''
+
+    tweetsData.forEach(function(tweet){
+
+        let likeIconClass = ''
+
+        if (tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+
+        let retweetIconclass = ''
+
+        if (tweet.isRetweeted){
+            retweetIconclass = 'retweeted'
+        }
+
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart ${likeIconClass}"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+    <div class="hidden" id="replies-${tweet.uuid}">
+        ${repliesHtml}
+    </div>   
+</div>
+`
+
+    })
+
+    return feedHtml
+}
 
 function render(){
     document.getElementById('feed').innerHTML = getFeedHtml()
