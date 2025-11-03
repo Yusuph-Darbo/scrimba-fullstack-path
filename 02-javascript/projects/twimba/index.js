d3b8b78f64380d8b7f3436a0ba1b62aa902a81f9
Yusuph <yusuphdarbo@gmail.com>
2025-11-03 09:18:13 +0000

    Created render function

--- 02-javascript/projects/twimba/index.js
+++ 02-javascript/projects/twimba/index.js
@@ -1,2 +1,8 @@
 import { tweetsData } from './data.js';
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render ()
