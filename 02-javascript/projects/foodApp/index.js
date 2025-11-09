f19846dfbc3cfdf50359f995e7d6267b8ed34866
Yusuph <yusuphdarbo@gmail.com>
2025-11-09 14:55:18 +0000

    Creating html boilerplate in cs

--- 02-javascript/projects/foodApp/index.js
+++ 02-javascript/projects/foodApp/index.js
@@ -1,3 +1,27 @@
-const orderSection = document.getElementsByName('main')
+import { menuArray } from "./data.js"
 
-console.log(orderSection)
+function getFeedHtml(){
+    let menuHtml = ''
+    
+    menuArray.forEach(item => {
+        menuHtml += `
+            <div class="menu-item">
+                <div class="menu-item-emoji">${item.emoji}</div>
+                <div class="menu-item-details">
+                    <h3 class="menu-item-name">${item.name}</h3>
+                    <p class="menu-item-ingredients">${Array.isArray(item.ingredients) ? item.ingredients.join(', ') : item.ingredients}</p>
+                    <p class="menu-item-price">$${item.price}</p>
+                </div>
+                <button class="add-btn" data-id="${item.id}">+</button>
+            </div>
+        `
+    })
+    
+    return menuHtml
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
