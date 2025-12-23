b483ed487e80cbe273033eaeade86b7ed9ac1755
Yusuph <yusuphdarbo@gmail.com>
2025-12-23 16:10:12 +0000

    created about & interests components

---
+++ 03-react/business-card/src/components/about.jsx
@@ -0,0 +1,10 @@
+export default function About (){
+    return (
+        <>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Blanditiis cum perspiciatis itaque, asperiores laboriosam
+             saepe quae nam ducimus illo tempora expedita</p>
+        </>
+    )
+}
---
+++ 03-react/business-card/src/components/interests.jsx
@@ -0,0 +1,10 @@
+export default function Interests (){
+    return (
+        <>
+        <h2>Interests</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Blanditiis cum perspiciatis itaque, asperiores laboriosam
+             saepe quae nam ducimus illo tempora expedita</p>
+        </>
+    )
+}
