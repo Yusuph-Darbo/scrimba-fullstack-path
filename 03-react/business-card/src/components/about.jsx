49f46defda40ea6c8e82a00999f7003a00bd2e64
Yusuph <yusuphdarbo@gmail.com>
2025-12-23 17:39:37 +0000

    minor text addition

--- 03-react/business-card/src/components/about.jsx
+++ 03-react/business-card/src/components/about.jsx
@@ -3,7 +3,7 @@ export default function About (){
         <>
         <h2>About</h2>
         <p>A CS student at Technological University Dublin (TUD), currently learning React, Express and API development.
-            Having fun building backend, frontend and full-stack projects, can't wait for my next project! </p>
+            Having fun building backend, frontend, CLI tools and full-stack projects, can't wait for my next project! </p>
         </>
     )
 }
