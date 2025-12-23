5016ac14fa95c4e45d9a30a4cb42e23aa53d3677
Yusuph <yusuphdarbo@gmail.com>
2025-12-23 16:08:26 +0000

    created info component

---
+++ 03-react/business-card/src/components/info.jsx
@@ -0,0 +1,19 @@
+export default function Info(){
+    return (
+        <>
+            <img src="./assets/spiderman.jpeg"/>
+            <h1>Laura Smith</h1>
+            <h3>Passionate Developer</h3>
+            <div class="buttons">
+                <button>
+                    <FontAwesomeIcon icon={byPrefixAndName.fas['envelope']} />
+                    Email
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={byPrefixAndName.fab['linkedin']} />
+                    Email
+                </button>
+            </div>
+        </>
+    )
+}
